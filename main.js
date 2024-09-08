6fdbc5564ba25c4500d2539fed15854ac2bcda67
Rana Almalki <ranaalmalki12@gmail.com>
2024-09-08 12:01:40 +0300

    solve the six question

--- main.js
+++ main.js
@@ -40,3 +40,14 @@ var tip = bill* 0.15;
 var total= bill + tip;
 
 console.log("bill is "+bill+" tip is"+tip+" the total is "+total);
+
+
+
+
+//6 
+
+var adjective1 = "amazing";
+var adjective2 = "fun";
+var adjective3 = "entertaining";
+console.log('The Intro to JavaScript course is '+adjective1+'. James and Julia are so '+adjective1+' I cannot wait to work through the rest of this'+ adjective3+' content!');
+
