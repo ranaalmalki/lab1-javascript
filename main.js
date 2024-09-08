56227f89bb28c15ad4271bf93ceda40cdcd43aaf
Rana Almalki <ranaalmalki12@gmail.com>
2024-09-08 11:44:52 +0300

    solve the third question

--- main.js
+++ main.js
@@ -8,6 +8,16 @@ console.log("The area of the rectangle is:"+result)
 
 
 
+// 2-محول درجة الحرارى
+let  c = 40;
+let feh =  (c * 9/5)+32
+console.log(c+'C is'+feh+"F");
 
 
+//3- من ساعه  الى ثانيه 
+
+let houre =2;
+let second = houre*3600
+
+console.log(houre+" hours =" +second+ "Seconds")
 
