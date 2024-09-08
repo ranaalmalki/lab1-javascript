566963feb3dfa65f979da8a150b4e5c4e6205e32
Rana Almalki <ranaalmalki12@gmail.com>
2024-09-08 11:49:08 +0300

    solve the fourth question

--- main.js
+++ main.js
@@ -21,3 +21,18 @@ let second = houre*3600
 
 console.log(houre+" hours =" +second+ "Seconds")
 
+
+/*4
+ * REQUIREMENTS
+ * 1. Your code should have two variables -  `thingOne` and `thingTwo`
+ * 2. Your code should only have one `console.log` statement
+ * 3. Your code should print out the values of `thingOne` and `thingTwo` using concatenation
+ * 4. Your code should contain semicolons at the end of each line
+ */
+
+//4-Concatenation
+
+let thingOne = "demah";
+let thingTwo= "is the best";
+let conct = thingOne.concat(" ",thingTwo )
+console.log(conct)
